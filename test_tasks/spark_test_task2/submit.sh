b5e01dbb84f30dc96a3f775d8254aacfd745d806
wang,xiaoyu <wangxiaoyu.au@gmail.com>
2020-05-29 13:52:26 +1000

    Update to use maximum memory

--- test_tasks/spark_test_task2/submit.sh
+++ test_tasks/spark_test_task2/submit.sh
@@ -4,5 +4,7 @@ spark-submit \
     --master spark://192.168.122.190:7077 \
     --deploy-mode client \
     --num-executors 4 \
+    --executor-memory 2867m \
+    --driver-memory 2867m \
     --py-files func_utils.py classfication.py \
     --input hdfs://192.168.122.247:9000/spark_test/data/train.tsv
