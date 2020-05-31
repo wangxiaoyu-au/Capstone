6d0d31c263c9e56a16e913578b4c57bebd6f8a14
wang,xiaoyu <wangxiaoyu.au@gmail.com>
2020-05-31 22:03:21 +1000

    Update launch script for task6

--- test_tasks/spark_test_task6/submit-8-cores.sh
+++ test_tasks/spark_test_task6/submit-8-cores.sh
@@ -1,10 +1,15 @@
+hadoop fs -rm -f hdfs://192.168.122.247:9000/spark_test/task6/simple-project_2.11-1.0.jar
+hadoop fs -mkdir hdfs://192.168.122.247:9000/spark_test/task6/
+hadoop fs -put simple-project_2.11-1.0.jar hdfs://192.168.122.247:9000/spark_test/task6/simple-project_2.11-1.0.jar
+
 spark-submit \
     --master spark://192.168.122.190:7077 \
-    --deploy-mode client \
+    --deploy-mode cluster \
+    --class ClassifierApp \
     --num-executors 4 \
     --executor-cores 8 \
     --executor-memory 2g \
     --driver-memory 2g \
     --class ClassifierApp \
-    ./simple-project_2.11-1.0.jar \
-    --input hdfs://192.168.122.247:9000/spark_test/data/train.tsv
+    hdfs://192.168.122.247:9000/spark_test/task6/simple-project_2.11-1.0.jar \
+    --input  hdfs://192.168.122.247:9000/spark_test/data/train.tsv
