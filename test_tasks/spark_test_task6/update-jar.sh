e697fde224326fc463087fa8324ae032aef275ec
wang,xiaoyu <wangxiaoyu.au@gmail.com>
2020-06-14 15:03:56 +1000

    Update script for updating jar

--- test_tasks/spark_test_task6/update-jar.sh
+++ test_tasks/spark_test_task6/update-jar.sh
@@ -1,3 +1,6 @@
+cd src
+./build.sh
+cd ..
 hadoop fs -rm -f hdfs://192.168.122.247:9000/spark_test/task6/simple-project_2.11-1.0.jar
 hadoop fs -mkdir hdfs://192.168.122.247:9000/spark_test/task6/
 hadoop fs -put simple-project_2.11-1.0.jar hdfs://192.168.122.247:9000/spark_test/task6/simple-project_2.11-1.0.jar
